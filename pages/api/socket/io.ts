7c0ec3e7c1302e490a7d6defd64873a1006caf4c
Ilyass <kreilyass@gmail.com>
2024-04-06 12:55:50 +0000

    change7

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -1,6 +1,6 @@
 import { Server as NetServer } from "http";
 import { NextApiRequest } from "next";
-import { Server as HttpsServer } from "https";
+
 import { Server as ServerIO } from "socket.io";
 
 import { NextApiResponseServerIo } from "@/lib/utils";
@@ -14,7 +14,7 @@ export const config = {
 const ioHandler = (req:NextApiRequest,res:NextApiResponseServerIo) => {
     if(!res.socket.server.io) {
         const path = "/api/socket/io"
-        const httpServer:HttpsServer = res.socket.server as any
+        const httpServer:NetServer = res.socket.server as any
         const io = new ServerIO(httpServer,{
             path:path,
             addTrailingSlash:false
--- src/assets/other/providers/socket-provider.tsx
+++ src/assets/other/providers/socket-provider.tsx
@@ -1,10 +1,9 @@
 "use client"
 
 import { useState,useEffect,useContext,createContext } from "react"
-import { useSession } from "next-auth/react"
 import {io as ClientIO} from "socket.io-client"
 import { changeUserState } from "@/lib/actions/user.action"
-import axios from "axios"
+//import axios from "axios"
 type SocketContextType = {
     socket:any | null;
     isConnected:boolean
@@ -27,7 +26,6 @@ export const SocketProvider = ({
 }) => {
     const [socket,setSocket] = useState(null)
     const [isConnected,setIsConnected] = useState(false)
-    const {data:session} = useSession()
     useEffect(() => {
         
         const socketInstance = new (ClientIO as any)(process.env.NEXT_PUBLIC_SITE_URL!,{
@@ -36,27 +34,27 @@ export const SocketProvider = ({
         });
         socketInstance.on('connect',() => {
             setIsConnected(true)
-            updateUserState(session?.user?.email,"online")
+           /* updateUserState(session?.user?.email,"online")*/
             
         })
         socketInstance.on('disconnect',() => {
             setIsConnected(false)
 
-            updateUserState(session?.user?.email,"offline")
+           /* updateUserState(session?.user?.email,"offline")*/
             
         })
         
         
         
-        const handleUnload = (e:BeforeUnloadEvent) => {
+        /*const handleUnload = (e:BeforeUnloadEvent) => {
             updateUserState(session?.user?.email,"offline")  
             
         }
-        window.addEventListener('beforeunload',handleUnload)
+        window.addEventListener('beforeunload',handleUnload)*/
         setSocket(socketInstance)
 
         return () => {
-           window.removeEventListener("beforeunload", handleUnload);
+           /*window.removeEventListener("beforeunload", handleUnload);*/
            
             socketInstance.disconnect();
         }
@@ -72,7 +70,7 @@ export const SocketProvider = ({
 }
 
 
-const updateUserState = async(email:string|null|undefined,state:string) => {
+/*const updateUserState = async(email:string|null|undefined,state:string) => {
     try {
         
        await axios.post("/api/socket/status",{
@@ -85,4 +83,4 @@ const updateUserState = async(email:string|null|undefined,state:string) => {
     } catch (error:any) {
         throw new Error(`Failed to change ${error.message}`)
     }
-}
+}*/
